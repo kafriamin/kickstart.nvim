fdc1c4f630e0a4232a15a1afca6a893237d5dd38
kafriamin <kafriamin2145@gmail.com>
2024-06-08 20:39:22 +0800

    added leader tp for fun

--- init.lua
+++ init.lua
@@ -97,6 +97,8 @@ vim.keymap.set('n', '<leader>p', function()
   vim.cmd 'Git push'
 end)
 
+vim.keymap.set('n', '<leader>tp', vim.cmd.TransparentToggle)
+
 -- Set highlight on search, but clear on pressing <Esc> in normal mode
 -- vim.opt.hlsearch = true
 -- vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
